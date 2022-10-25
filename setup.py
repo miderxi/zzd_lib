767c4bf2afe1c1e37684f4c6aa0bbf82659e70b8
miderxi <1965770446@qq.com>
2022-10-25 09:58:14 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zzd",
-    version="0.3.34",
+    version="0.3.35",
     author="zzd lab", 
 	author_email="1965770446@qq.com",
     description="encode and scores", 
